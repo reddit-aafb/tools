f60939a4b788d58aaf4636cf2cc2a728590da36a
Jonas Häggqvist <rasher@rasher.dk>
2019-02-10 20:07:38 +0100

    Sidebar can now actually update the sidebar

--- sidebarupdater.py
+++ sidebarupdater.py
@@ -13,13 +13,16 @@
 #  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
 #  OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 #  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
+import difflib
+import re
 
 import pendulum
+from praw import Reddit
 
 from aafclient import AAFClient
 from helpers import RenderHelper
 from redditdata import subreddits
+from reddittoken import ensure_scopes
 
 
 def make_schedule_ctx(week, games):
@@ -37,13 +40,29 @@ def make_schedule_ctx(week, games):
     return ctx
 
 
+def marker_replace(marker_start, marker_end, content, subject):
+    replacement = "%s\n\n%s\n\n%s" % (marker_start, content, marker_end)
+    return re.sub(r'(?ms)' + re.escape(marker_start) + '.*?' + re.escape(marker_end), replacement, subject)
+
+
 def main():
+    import sys
     renderer = RenderHelper()
     aaf = AAFClient('aaf_standings;reddit.com/r/aafb')
     week, games = aaf.schedule(for_week=pendulum.now())
     ctx = make_schedule_ctx(week, games)
     schedule = renderer.render('schedule.md', ctx)
-    print(schedule)
+
+    r = Reddit('aaf_robot')
+    ensure_scopes(r)
+
+    sub = r.subreddit(sys.argv[1])
+    old_sidebar = sub.description
+    new_sidebar = marker_replace('#### [](/blank "START schedule")', '#### [](/blank "END schedule")', schedule,
+                                 old_sidebar)
+    if old_sidebar != new_sidebar:
+        print("\n".join(difflib.unified_diff(old_sidebar.split("\n"), new_sidebar.split("\n"), fromfile='old_sidebar_%s' % sub.display_name, tofile='new_sidebar_%s' % sub.display_name, n=0, lineterm="")))
+        sub.mod.update(description=new_sidebar)
 
 
 if __name__ == '__main__':
